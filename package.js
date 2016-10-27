73698901f27596d2d26a0c706e74b018a14dcd2a
Nathan Muir <ndmuir@gmail.com>
2016-10-27 10:26:52 +1000

    Release 4.14.30_4 (Compatible with Meteor 1.4)

--- package.js
+++ package.js
@@ -1,18 +1,18 @@
 Package.describe({
   name: '3stack:react-datetimepicker',
-  version: '4.14.30_3',
+  version: '4.14.30_4',
   summary: 'A simple react helper for <ReactDatetimePicker>',
   git: 'https://github.com/3stack-software/meteor-react-datetimepicker',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api){
-  api.versionsFrom('METEOR@1.2');
+  api.versionsFrom('METEOR@1.4');
   api.export('ReactDatetimePicker', 'client');
   api.imply('3stack:datetimepicker-common@4.14.30');
   api.use([
     'meteor-base',
-    'react@0.1.13',
+    'react@15.0.1',
     'tsega:bootstrap3-datetimepicker@4.14.30_4',
     '3stack:datetimepicker-common@4.14.30'
   ], 'client');
